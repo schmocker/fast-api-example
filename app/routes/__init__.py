4fec8ac6dda1532933a5210ecf37b296cdba16c5
Tobias Schmocker <tobias.schmocker@axpo.com>
2023-03-01 16:25:30 +0100

    empty branch for live demo

--- app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .admin import route as admin_route
-from .asset import route as asset_route
-from .project import route as project_route
-from .project_asset_mapping import route as project_asset_mapping_route
